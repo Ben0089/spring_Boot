504cce452b5eae4a1a139cd20dc7a9c216efe427
DESKTOP-UT6PUAP\Ben Skywalker <eess56789a@gmail.com>
2024-02-23 16:23:31 +0800

    finished the whole project

--- src/main/java/com/example/demo1/student/StudentService.java
+++ src/main/java/com/example/demo1/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo1.student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -43,5 +45,18 @@ public class StudentService {
         studentRepository.deleteById(studentId);
         }
 
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId).orElseThrow(
+                () -> new IllegalStateException(
+                        "student with " + studentId + " does not exists"
+                ));
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(),name)) {
+            student.setName(name);
+        }
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(),email)) {
+            student.setEmail(email);
+        }
 
+    }
 }
